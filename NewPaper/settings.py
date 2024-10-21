7e931ed7f9ddad3ef3aed8c12d381992f74f4e5d
Anton Bliznuk <antonbliznuk71@gmail.com>
2024-10-21 15:46:21 +0300

    Fixes problems with static files

--- NewPaper/settings.py
+++ NewPaper/settings.py
@@ -23,9 +23,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Добавьте пути к статическим файлам каждого приложения
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main', 'static'),
-    os.path.join(BASE_DIR, 'posts', 'static'),
-    os.path.join(BASE_DIR, 'profiles', 'static'),
+    os.path.join(BASE_DIR, 'main/static'),
+    os.path.join(BASE_DIR, 'posts/static'),
+    os.path.join(BASE_DIR, 'profiles/static'),
 ]
 
 
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
