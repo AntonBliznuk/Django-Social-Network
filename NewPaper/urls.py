1ce7f216e1ee71215c8bde3443c6f9c77fad19dd
Anton Bliznuk <antonbliznuk71@gmail.com>
2024-10-22 14:49:11 +0300

    Fix media problems

--- NewPaper/urls.py
+++ NewPaper/urls.py
@@ -17,12 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('profile/', include('profiles.urls')),
     path('post/', include('posts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
